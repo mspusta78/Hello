dcdf84ec0a8b949279fe3c6c24be33d6752efee9
mspusta78 <martinspusta@gmail.com>
2021-05-26 12:42:51 -0400

    update

--- server.js
+++ server.js
@@ -1,38 +1,5 @@
 var http = require('http'); // Import Node.js core module
 
-// var server = http.createServer(function (req, res) {   //create web server
-//     if (req.url == '/') { //check the URL of the current request
-                
-//         // set response header
-//         res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
-//         // set response content    
-//         res.write('<html><body><p>This is home Page.</p></body></html>');
-//         res.end();    
-//     }
-//     else if (req.url == "/student") {            
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<html><body><p>This is student Page.</p></body></html>');
-//         res.end();
-
-//     }
-//     else if (req.url == "/teacher") {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<html><body><p>This is teacher Page.</p></body></html>');
-//         res.end();
-
-//     }
-//     else{
-//         res.end('Invalid Request!');
-//     }
-// });
-
-// server.listen(5000); // listen for any incoming requests
-
-// console.log('Node.js web server is running at http://127.0.0.1:5000 ')
-
-
-
 var express = require('express');
 var app = express();
 
